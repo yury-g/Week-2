68e65bb2ab4be1e0c24f800e690dd44518607723
yury-g <yury.gitman@gmail.com>
2016-09-08 11:01:53 -0400

    Week 2

--- Week 2/ViewController.swift
+++ Week 2/ViewController.swift
@@ -5,14 +5,67 @@
 //  Created by Yury Gitman on 9/8/16.
 //  Copyright © 2016 com.yuryg. All rights reserved.
 //
+//  topics: Vars(Int, Float, String), Function Syntax, Operators (+, -, =), Slider, Labels, Buttons, ProgessView
+
 
 import UIKit
 
 class ViewController: UIViewController {
+   
+    //vars
+    var myString = "Hello"
+    var nameString = ""
+    var myFloat: Float = 0.0
+    
+    
+    @IBOutlet weak var myTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
 
+    @IBOutlet weak var myProgressView: UIProgressView!
+
+    @IBOutlet weak var myNumLabel: UILabel!
+    
+    @IBOutlet weak var mySlider: UISlider!
+    
+    
+    @IBAction func button1Pressed(sender: UIButton) {
+        nameString = myTextFieldOutlet.text!
+        
+       myLabel.text = myString + " " + nameString
+        myTextFieldOutlet.resignFirstResponder()
+    }
+    
+    @IBAction func TextEntered(sender: UITextField) {
+        // Example that this can be an action too.
+        print("textEntered")
+        print( myString + " " + nameString)
+    }
+
+    
+    @IBAction func mySliderMoved(sender: UISlider) {
+        print("slider Moved value is \(mySlider.value)")
+        
+        myFloat = mySlider.value
+        myNumLabel.text = String(myFloat)
+        myProgressView.progress = myFloat
+    
+        
+    }
+    
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "what's your name?"
+        myNumLabel.text = String(myFloat)
+        myProgressView.progress = myFloat
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +74,10 @@ class ViewController: UIViewController {
     }
 
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
 }
 
